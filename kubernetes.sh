7871e3c7e2996d3923eb869c14612214ec007ca6
Tega Agbanagba <tegamike@gmail.com>
2020-06-03 20:39:24 +0100

    Initial Commit

---
+++ kubernetes.sh
@@ -0,0 +1,57 @@
+
+$CRI_VERSION = 1.3.4
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# install zip
+sudo apt install -y zip
+
+# Container runtime - containerd
+
+mkdir containerd
+wget https://github.com/containerd/containerd/releases/download/v$CRI_VERSION/containerd-${CRI_VERSION}.linux-amd64.tar.gz
+tar -xvf containerd-$CRI_VERSION.linux-amd64.tar.gz -C containerd
+sudo mv containerd/bin/* /bin
+
+cat <<EOF | sudo tee /etc/systemd/system/containerd.service
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target
+
+[Service]
+ExecStartPre=/sbin/modprobe overlay
+ExecStart=/bin/containerd
+Restart=always
+RestartSec=5
+Delegate=yes
+KillMode=process
+OOMScoreAdjust=-999
+LimitNOFILE=1048576
+LimitNPROC=infinity
+LimitCORE=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF | sudo tee /etc/containerd/config.toml
+[plugins]
+  [plugins.cri.containerd]
+    snapshotter = "overlayfs"
+    [plugins.cri.containerd.default_runtime]
+      runtime_type = "io.containerd.runtime.v1.linux"
+      runtime_engine = "/usr/local/bin/runc"
+      runtime_root = ""
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable containerd
+sudo systemctl start containerd
